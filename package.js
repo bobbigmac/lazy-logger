1309ddd8ad7874d8ecbb4e6f9a9a393e41f08367
bobbigmac <admin@bobbigmac.com>
2015-09-02 01:30:23 +0100

    Version set for release

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'bobbigmac:lazy-logger',
-  version: '0.1.0',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
   summary: 'Simple logging wrapper for client-side to display console.log, console.warn and console.error messages in the page body (reduce need to have the console open)',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/bobbigmac/lazy-logger',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
