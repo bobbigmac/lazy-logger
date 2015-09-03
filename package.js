da6a9c31ffeb3bba9f062f68335e58febe0290f3
bobbigmac <admin@bobbigmac.com>
2015-09-03 18:49:06 +0100

    Set debugOnly

--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
   name: 'bobbigmac:lazy-logger',
-  version: '1.0.1',
+  version: '1.0.2',
   // Brief, one-line summary of the package.
   summary: 'Display console.log, .warn and .error messages in the page body (reduce need for console)',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/bobbigmac/lazy-logger',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: 'README.md',
+  debugOnly: true
 });
 
 Package.onUse(function(api) {
